79a7751f458126d440cf509d13daee4ca6b63e70
asep <asepsunarya839@gmail.com>
2025-01-16 04:36:27 +0700

    add: docker file

---
+++ Dockerfile
@@ -0,0 +1,45 @@
+FROM php:8.2-fpm
+
+# Install dependencies
+RUN apt-get update && apt-get install -y \
+    build-essential \
+    libpng-dev \
+    libjpeg-dev \
+    libfreetype6-dev \
+    locales \
+    zip \
+    jpegoptim optipng pngquant gifsicle \
+    vim \
+    unzip \
+    git \
+    curl \
+    libonig-dev \
+    libzip-dev \
+    zip \
+    libmcrypt-dev \
+    libxml2-dev
+
+# Clear cache
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Install extensions
+RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd zip
+
+# Install Composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+# Set working directory
+WORKDIR /var/www/html
+
+# Copy existing application directory contents
+COPY . /var/www/html
+
+# Copy existing application directory permissions
+COPY --chown=www-data:www-data . /var/www/html
+
+# Change current user to www
+USER www-data
+
+# Expose port 8000 and start php-fpm server
+EXPOSE 8000
+CMD ["php-fpm"]
--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -5,6 +5,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class UserRequest extends FormRequest
 {
@@ -15,13 +16,24 @@ class UserRequest extends FormRequest
 
     public function rules()
     {
-        return [
+        $adminId = $this->route('id');
+        $rules = [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email',
+            'email' => [
+                'required',
+                'email',
+                Rule::unique('users', 'email')->ignore($adminId),
+            ],
             'ktp' => 'required|string|max:16',
             'phone' => 'required|string|max:14',
-            'password' => 'required|string|min:6',
         ];
+
+        if ($this->isMethod('post')) {
+            $rules['password'] = 'required|string|min:6';
+        } elseif ($this->isMethod('put') || $this->isMethod('patch')) {
+            $rules['password'] = 'nullable|string|min:6';
+        }
+        return $rules;
     }
 
     protected function failedValidation(Validator $validator)
